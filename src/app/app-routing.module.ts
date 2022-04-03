3eb0fda4eb3a3f082f339e51c7f749b3ff86f686
vinczeadam01 <adam.vincze01@gmail.com>
2022-04-03 19:54:57 +0200

    Install angular material, generete login and signup modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/shared/login/login.module').then(m => m.LoginModule) }, { path: 'signup', loadChildren: () => import('./pages/shared/signup/signup.module').then(m => m.SignupModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
