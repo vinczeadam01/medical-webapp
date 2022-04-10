e4ada7ed0dcc41834c6d81d12c26c2e7170077df
vinczeadam01 <adam.vincze01@gmail.com>
2022-04-10 12:11:14 +0200

    auth.sevice isLoggedIn method

--- src/app/shared/services/auth.guard.ts
+++ src/app/shared/services/auth.guard.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const user = JSON.parse(localStorage.getItem('user') as string);
-      if(user) {
-        return true;
-      }
-      this.router.navigateByUrl("/login");
-      return false;
+
+    if(this.authService.isLoggedIn) {
+      return true;
+    }
+    console.error("Elérés megtagadva!");
+    this.router.navigateByUrl("/login");
+    
+    return false;
   }
-  
+
 }
