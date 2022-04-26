16a85b90c6df678efe25ce6503e140bec67fbebf
vinczeadam01 <adam.vincze01@gmail.com>
2022-04-26 22:50:07 +0200

    canvasJS chart

--- src/app/patient/pages/analitycs/analitycs.component.ts
+++ src/app/patient/pages/analitycs/analitycs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from 'src/app/shared/models/patient';
+import { MeasureService } from 'src/app/shared/services/measure.service';
+import * as CanvasJS from '../../../../assets/canvasjs.min'
 
 @Component({
   selector: 'app-analitycs',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalitycsComponent implements OnInit {
 
-  constructor() { }
+  patient: Patient;
+
+  constructor(private measurementService: MeasureService) {
+    this.patient = JSON.parse(localStorage.getItem('patient') as string);
+   }
 
   ngOnInit(): void {
-  }
+    let dataPoints: any = [];
+
+    this.measurementService.getByUserId(this.patient.id).subscribe(datas => {
+      for(const data of datas) {
+        dataPoints.push({x: data.dia, y: data.sys});
+      }
+    })
+
+    console.log(dataPoints);
+    
+    let chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      title: {
+        text: "Vérnyomás diagram"
+      },
+      data: [
+      {
+        type: "line",                
+        dataPoints: dataPoints
+      }]
+    });
+      
+    chart.render();
+      }
 
 }
